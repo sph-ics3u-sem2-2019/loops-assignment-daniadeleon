acf1fe19486c14ab1eb831932bc4eb10a358718c
daniadeleon <47532261+daniadeleon@users.noreply.github.com>
2019-03-26 09:54:52 -0400

    stuff

--- src/problem1/Main.java
+++ src/problem1/Main.java
@@ -5,6 +5,15 @@ public class Main {
 	public static void main(String[] args) {
 		// Read a value. Print out from 1 to the input
 		Scanner in=new Scanner(System.in);
+		int counter=1;
+		System.out.println("Give me a value");
+		int max=in.nextInt();
+		while (counter<=max) {
+			System.out.println(counter);
+			counter++;
+		
+		
 	}
-
+	
+	}
 }
