f04946b9f122170ecf3a769333441cf8f52c8168
daniadeleon <47532261+daniadeleon@users.noreply.github.com>
2019-03-27 11:16:50 -0400

    Almost done

--- src/problem2/Main.java
+++ src/problem2/Main.java
@@ -5,7 +5,15 @@ public class Main {
 	public static void main(String[] args) {
 		// Read in two numbers. Add up all the values between them
 		Scanner in=new Scanner(System.in);
-
+		int counter=1;
+		int max=1;
+		System.out.println("Give me the first number");
+		counter=in.nextInt();
+		System.out.println("Give me the second number");
+		max=in.nextInt();
+		while (counter<=max) { 
+				System.out.println(counter);
+				counter++;
 	}
-
+	}
 }
